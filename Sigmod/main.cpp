3b1eb29ade101225c390fed53431945580cc78d8
Jianwei Xu <jianwei.xu@mail.utoronto.ca>
2013-05-02 19:23:16 -0400

    Initial Commit

---
+++ Sigmod/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sigmod
+//
+//  Created by Jianwei Xu on 02/05/2013.
+//  Copyright (c) 2013 Jianwei Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
